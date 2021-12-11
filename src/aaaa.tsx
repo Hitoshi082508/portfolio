79be0fcf23243ac5e60270d5d61121890fde7d54
hitoshi082508 <hitoshi082508@gmail.com>
2021-12-11 22:37:55 +0900

    setup styled-component

---
+++ src/aaaa.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+type Props = {
+  className?: string
+}
+export const FCAAA: React.FC<Props> = ({ className }) => {
+  return <div className={className}>AAA</div>
+}
+export const AAA = styled(FCAAA)``
